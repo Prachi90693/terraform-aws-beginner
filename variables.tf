7c957d823ca90e54524e61b9df0eb6e6528ebf21
Prachi90693 <prachisharma309709@gmail.com>
2026-06-13 19:05:33 +0530

    Add variables.tf with parameterised inputs for region, environment, and instance type

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+  default     = "terraform-demo"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance (Amazon Linux 2 in ap-south-1)"
+  type        = string
+  default     = "ami-03f4878755434977f"
+}
