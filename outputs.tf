b9704bce6b059c0d43348b1e8d5d7030d1f44fe7
Prachi90693 <prachisharma309709@gmail.com>
2026-06-13 19:06:06 +0530

    Add outputs.tf to expose VPC, subnet, security group, EC2, and IAM role IDs

---
+++ outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "security_group_id" {
+  description = "ID of the web security group"
+  value       = aws_security_group.web.id
+}
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "iam_role_arn" {
+  description = "ARN of the IAM role attached to EC2"
+  value       = aws_iam_role.ec2_role.arn
+}
